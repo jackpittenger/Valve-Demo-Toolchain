3a45e12d3dbdffc78784a86dba6f4a0c0b53dcb1
realSaddy <jrp30074@gmail.com>
2020-12-22 21:04:49 -0600

    Auto restart on crash for GNU/Linux

--- obs.py
+++ obs.py
@@ -3,6 +3,7 @@ import os
 import time
 import glob
 import threading
+import subprocess
 import obspython as obs
 
 run = False
@@ -11,6 +12,7 @@ rf = ""
 cb = False
 df = ""
 af = ""
+al = False
 
 def script_description():
     return "Demo2Obs by https://github.com/realSaddy"
@@ -33,21 +35,30 @@ def script_properties():
     obs.obs_property_set_long_description(cb_b, "Enabling will move recorded .dem, .json, & .vdm files to the archive folder directory to avoid double recording")
     obs.obs_property_set_modified_callback(cb_b, cb_pressed)
     
+    # Auto restart
+    al_b = obs.obs_properties_add_bool(props, "al", "(LINUX ONLY) Auto relaunch TF2 demo recording on crash?")
+    obs.obs_property_set_long_description(al_b, "Currently only supports GNU/Linux. Will attempt to auto restart demo recording if TF2 crashes on the next clip")
+    obs.obs_property_set_modified_callback(al_b, al_pressed)
+    
     df = obs.obs_properties_add_text(props, "df", "Demo folder", obs.OBS_TEXT_DEFAULT)
     obs.obs_property_set_long_description(df, "The folder containing your .dem files")
-    visible_prop(props, "df", cb)
+    visible_prop(props, "df", al or cb)
 
     af = obs.obs_properties_add_text(props, "af", "Archive folder", obs.OBS_TEXT_DEFAULT)
     obs.obs_property_set_long_description(af, "The folder to move completed .dem files")
     visible_prop(props, "af", cb)
 
-    b = obs.obs_properties_add_button(props,"b1","Start", b_start)
-    
+    obs.obs_properties_add_button(props,"b1","Start", b_start)
+
     return props
 
 def cb_pressed(props, prop, *arg, **kwargs):
-    visible_prop(props, "df", cb)
+    visible_prop(props, "df", al or cb)
     visible_prop(props, "af", cb)
+    return True
+
+def al_pressed(props, prop, *arg, **kwargs):
+    visible_prop(props, "df", al or cb)
     return True
 
 def b_start(props, prop, *arg, **kwargs):
@@ -60,6 +71,7 @@ def b_start(props, prop, *arg, **kwargs):
     visible_prop(props, "cb", False)
     visible_prop(props, "df", False)
     visible_prop(props, "af", False)
+    visible_prop(props, "al", False)
 
     global run
     run = True
@@ -77,6 +89,7 @@ def b_stop(props, prop, *arg, **kwargs):
     visible_prop(props, "cb", True)
     visible_prop(props, "df", True)
     visible_prop(props, "af", True)
+    visible_prop(props, "al", True)
     
     global run 
     run = False
@@ -89,12 +102,14 @@ def script_update(settings):
     global cb
     global df
     global af
+    global al
 
     wf = obs.obs_data_get_string(settings, "wf")
     rf = obs.obs_data_get_string(settings, "rf")
     cb = obs.obs_data_get_bool(settings, "cb")
     df = obs.obs_data_get_string(settings, "df")
     af = obs.obs_data_get_string(settings, "af")
+    al = obs.obs_data_get_bool(settings, "al")
 
 def move_file(clip_name):
     file_types = [".dem", ".json", ".vdm"]
@@ -114,7 +129,16 @@ def move_file(clip_name):
 
 def busy_thread():
     obs.obs_frontend_recording_stop()
+    last_demo_processed = ""
     while run:
+        if al and last_demo_processed != "":
+            try:
+                subprocess.check_output("pidof hl2_linux", shell=True)
+            except subprocess.CalledProcessError:
+                print("Attempting to auto restart TF2")
+                obs.obs_frontend_recording_stop()
+                subprocess.call("steam -applaunch 440 +playdemo "+df+"/"+last_demo_processed, shell=True)
+                time.sleep(6)
         if not os.path.exists(wf):
             print("--ERROR: Watch folder doesn't exist!")
         elif not os.path.exists(rf):
@@ -124,6 +148,7 @@ def busy_thread():
             if(len(res) > 0):
                 print("RECSTART Detected")
                 os.remove(wf+"/"+res[0])
+                last_demo_processed = "_".join(res[0].split(".")[1].split("_")[:2])
                 obs.obs_frontend_recording_start()
             res = [fn for fn in os.listdir(wf) if fn.startswith("recstop.")]
             if len(res) > 0:
@@ -133,7 +158,9 @@ def busy_thread():
                     obs.obs_frontend_recording_stop()
                     newest = max(glob.glob(rf+"/*"), key=os.path.getctime)
                     newsplit = newest.split("/")
-                    filename = "/".join(newsplit[:-1]) + "/" + res[0].split(".")[1] + "." + newsplit[-1].split(".")[1]
+                    n = res[0].split(".")[1]
+                    filename = "/".join(newsplit[:-1]) + "/" + n + "." + newsplit[-1].split(".")[1]
+                    last_demo_processed = "_".join(n.split("_")[:2])
                     os.rename(newest, filename)
                     print("Recorded "+filename)
                     if(cb):
